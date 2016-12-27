2c1a467425807aa1bf7985693295daedd0e65ae0
vitorepsantos <vitor.epsantos@gmail.com>
2016-12-27 11:17:41 -0200

    Update psql_utils.sql

--- psql_utils.sql
+++ psql_utils.sql
@@ -2,5 +2,9 @@
 psql -h <host_ip> -p <port> -U <dbuser> <dbname>
 
 # Restore a backup
-psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> < <bkp_file>
+psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> < <filepath>
 psql -h localhost -p 5432 -U mydbuser -d mylocaldb < E:\\mybkpfile.bkp
+
+# Save query result into file
+psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> -t -A -c "sql-query" > <filepath>
+psql -h localhost -p 5432 -U mydbuser -d mylocaldb -t -A -c "SELECT * FROM mytable;" > E:\\myqueryresult.txt
