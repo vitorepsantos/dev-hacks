623c7af5181f85d9dcbfcea1e097108572e1b4a3
vitorepsantos <vitor.epsantos@gmail.com>
2016-12-27 10:09:11 -0200

    Update psql_utils.sql

--- psql_utils.sql
+++ psql_utils.sql
@@ -1,2 +1,6 @@
 # Connect to DB
 psql -h <host_ip> -p <port> -U <dbuser> <dbname>
+
+# Restore a backup
+psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> < <bkp_file>
+psql -h localhost -p 5432 -U mydbuser -d mylocaldb < E:\\mybkpfile.bkp
