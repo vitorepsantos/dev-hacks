325b5ba50b472a2784e43b17fd60fdd17de75aec
vitorepsantos <vitor.epsantos@gmail.com>
2016-12-28 10:22:32 -0200

    Update psql_utils.sql

--- psql_utils.sql
+++ psql_utils.sql
@@ -1,10 +1,15 @@
-# Connect to DB
+-- PostgreSQL 9.1+
+
+-- Connect to DB
 psql -h <host_ip> -p <port> -U <dbuser> <dbname>
 
-# Restore a backup
+-- Restore a backup
 psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> < <filepath>
 psql -h localhost -p 5432 -U mydbuser -d mylocaldb < E:\\mybkpfile.bkp
 
-# Save query result into file
+-- Save query result into file
 psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> -t -A -c "sql-query" > <filepath>
 psql -h localhost -p 5432 -U mydbuser -d mylocaldb -t -A -c "SELECT * FROM mytable;" > E:\\myqueryresult.txt
+
+-- SQL query to clean DB
+VACUUM FULL FREEZE VERBOSE;
