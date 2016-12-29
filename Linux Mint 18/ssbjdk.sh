10cabf23ecfea9e4e1625703c91e0f3833670f7d
vitorepsantos <vitor.epsantos@gmail.com>
2016-12-29 14:58:57 -0200

    Update ssbjdk.sh

--- Linux Mint 18/ssbjdk.sh
+++ Linux Mint 18/ssbjdk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-/*
- * Install & Setup JDK
- */
+# $ sudo sh ssbjdk.sh
+
+cd ~/Documents
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz
+tar -xf jdk-7u80-linux-x64.tar.gz
+rm -f jdk-7u80-linux-x64.tar.gz
+sudo mv ~/Documents/jdk1.7.0_80/ /opt/
+sudo echo "export JAVA_HOME=/opt/jdk1.7.0_80" >> /etc/environment
+sudo echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/environment
