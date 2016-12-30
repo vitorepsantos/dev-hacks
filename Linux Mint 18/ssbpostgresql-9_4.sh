eb39ccb521ba0957fd2bbac03de1dd6ede4ab2b2
vitorepsantos <vitor.epsantos@gmail.com>
2016-12-30 11:48:22 -0200

    Update ssbpostgresql-9_4.sh

--- Linux Mint 18/ssbpostgresql-9_4.sh
+++ Linux Mint 18/ssbpostgresql-9_4.sh
@@ -9,3 +9,14 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install postgresql-9.4
+
+sudo sed -i '93i host    all             postgres             127.0.0.1/32            trust' /etc/postgresql/9.4/main/pg_hba.conf
+
+# sudo nano /etc/postgresql/9.4/main/pg_hba.conf
+# line 92
+# from
+# host    all             all             127.0.0.1/32            md5
+# to
+# host    all             postgres             127.0.0.1/32            trust
+#sudo /etc/init.d/postgresql restart
+# sudo -u postgres psql postgres
