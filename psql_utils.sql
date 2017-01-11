fd6500fa6b0f80fb41050720a3cd4d41a6a0bd8c
Vitor Santos <vitor.santos_indra@inpe.br>
2017-01-11 13:22:11 -0200

    add script

--- psql_utils.sql
+++ psql_utils.sql
@@ -11,5 +11,9 @@ psql -h localhost -p 5432 -U mydbuser -d mylocaldb < E:\\mybkpfile.bkp
 psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> -t -A -c "sql-query" > <filepath>
 psql -h localhost -p 5432 -U mydbuser -d mylocaldb -t -A -c "SELECT * FROM mytable;" > E:\\myqueryresult.txt
 
+-- Script
+psql -h <host_ip> -p <port> -U <dbuser> -d <dbname> -f <filepath.sql>
+psql -h localhost -p 5432 -U mydbuser -d mylocaldb -f /home/user/myscript.sql
+
 -- SQL query to clean DB
 VACUUM FULL FREEZE VERBOSE;
