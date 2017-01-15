eae3cfcce7b60f3403ddb9ccdc56cb6f587d0524
Vitor Santos <vitor.epsantos@gmail.com>
2017-01-15 11:34:16 -0200

    add ssbvbox5.1.12.sh

---
+++ Linux Mint 18/ssbvbox5.1.12.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Obrigado/Thanks http://www.edivaldobrito.com.br/virtualbox-no-linux/
+
+# uname -m
+# wget http://download.virtualbox.org/virtualbox/5.1.12/VirtualBox-5.1.12-112440-Linux_x86.run -O virtualbox.run
+
+wget http://download.virtualbox.org/virtualbox/5.1.12/VirtualBox-5.1.12-112440-Linux_amd64.run -O virtualbox.run
+chmod +x virtualbox.run
+sudo ./virtualbox.run
