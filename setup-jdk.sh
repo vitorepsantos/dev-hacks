239bda17237feaa15ca23e20d5a7d7794effb49d
Vitor Santos <vitor.epsantos@gmail.com>
2019-07-02 09:25:14 -0300

    Update setup-jdk.sh

--- setup-jdk.sh
+++ setup-jdk.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-
-sudo tar -xf jdk-8u111-linux-x64.tar.gz
-sudo rm -f jdk-8u111-linux-x64.tar.gz
-sudo nano /etc/environment
-# PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/bin"
-# JAVA_HOME="/opt/jdk1.8.0_111"
-sudo nano .bashrc
-# export PATH=$PATH:$JAVA_HOME/bin
+echo 'JAVA_HOME=\"/opt/jdk1.8.0_111\"' >> /etc/environment
+echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc
 source .bashrc
-java -version
 reboot
 java -version
